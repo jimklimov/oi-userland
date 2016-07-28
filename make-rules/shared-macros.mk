7bf637e5e7a6085f157fde745771f5ca3fb717d1
Jim Klimov <jim@jimklimov.com>
2016-07-28 14:55:42 +0200

    shared-macros.mk : just in case a user overrides COMPONENT_INSTALL_TARGETS - use it (what would be right: in addition to, or instead of, "install"?)

--- make-rules/shared-macros.mk
+++ make-rules/shared-macros.mk
@@ -434,7 +434,7 @@ $(BUILD_DIR_64)/.tested-and-compared:	BITS=64
 # created (although some components are known to limit themselves
 # to just building code with no actual installation implementation).
 ALL_INSTALLED_STAMP ?= $(BUILD_DIR)/.all-installed
-$(ALL_INSTALLED_STAMP): install
+$(ALL_INSTALLED_STAMP): install $(COMPONENT_INSTALL_TARGETS)
 	test -d "$(PROTO_DIR)" || { echo "WARN: install completed but PROTO_DIR is missing!">&2; }
 	$(TOUCH) $@
 
